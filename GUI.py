f034d63320e7c668a9642558519f4fd06d6e4dc6
Parth Patel <parth.b.patel5@gmail.com>
2022-05-06 18:54:35 -0500

    Regular Chatting finished

---
+++ GUI.py
@@ -0,0 +1,239 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.properties import ObjectProperty
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.popup import Popup
+from kivy.clock import Clock
+import socket
+import threading
+
+hostname = socket.gethostname()
+local_ip = socket.gethostbyname(hostname)
+
+client = None
+address = None
+
+class Manager(ScreenManager):
+    WelcomeScreen = ObjectProperty(None)
+    ChatScreen = ObjectProperty(None)
+
+
+class WelcomeScreen(Screen):
+    def on_enter(self):
+        global s, r, client, address
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # send connection, will recieve messages
+        r = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # recieve connection, will make client/address from
+        client, address = None, None
+        Clock.schedule_once(self.showIP)
+
+    def showIP(self, dt):
+        self.ids.ipLabel.text = local_ip
+
+
+    def aiBtn(self):
+        show_AIPop()
+
+    def sendBtn(self):
+        show_SendPop()
+
+    def waitBtn(self):
+        show_RecievePop()
+
+    def testBtn(self):
+        print("test")
+        self.manager.current = "chatscreen"
+
+
+
+
+
+class ChatScreen(Screen):
+    def on_enter(self, *args):
+        recThread = threading.Thread(target=self.reciever)
+        recThread.start()
+
+    def send(self):# sends messages
+        message = self.ids.inputBox.text
+        self.ids.inputBox.text = ""
+        self.ids.chat.text = self.ids.chat.text + local_ip +": " + message + "\n"
+        print(message)
+        client.send(message.encode())
+
+    def reciever(self):# recieves messages
+        end_connection = False
+        while not end_connection:
+            message = s.recv(1024).decode()
+            if len(message) > 0:
+                self.ids.chat.text = self.ids.chat.text + address[0] +": " + message + "\n"
+                print(message)
+            else:  # if conection closed
+                end_connection = True
+                print("-!-!-!-connection ended-!-!-!-")
+
+
+    def clearChat(self):
+        self.ids.chat.text = ""
+
+
+class InfoScreen(Screen):
+    pass
+
+
+class AIPop(FloatLayout):
+    pass
+
+
+def show_AIPop():
+    show = AIPop()
+
+    popupWindow = Popup(title="Connecting", content=show, size_hint=(None,None),size=(400,400))
+
+    popupWindow.open()
+
+
+class SendPop(FloatLayout):
+    otherIP = None
+    ref = None
+
+    def tBtn(self):
+        self.ids.enterBtn.disabled = True
+        self.ids.contBtn.disabled = False
+
+
+    def Btn(self):
+        self.otherIp = self.ids.Ip.text
+        t2 = threading.Thread(target=self.connect)
+
+        t2.start()
+
+        print(self.otherIp)
+        self.ids.Connecting.text = "Connecting..."
+        self.ids.enterBtn.disabled = True
+
+
+    def connect(self):
+        global client, address
+        s.connect((self.otherIp, 55555))
+
+        # recieve the connection, making sender socket
+        hostname = socket.gethostname()
+        local_ip = socket.gethostbyname(hostname)
+
+        r.bind((local_ip, 55556))
+        r.listen()
+
+        # conecting to a specific client
+        client_acquired = False
+        while not client_acquired:
+            client, address = r.accept()
+            if len(address) > 0:
+                if address[0] == self.otherIp:
+                    client_acquired = True
+
+        self.tBtn()
+
+
+    def dis(self):
+        print(self.ref)
+        self.ref.dismiss()
+
+
+def show_SendPop():
+    show = SendPop()
+
+    popupWindow = Popup(title="Connecting", content=show, size_hint=(None,None),size=(400,400))
+
+    show.ref = popupWindow
+    popupWindow.open()
+
+
+class RecievePop(FloatLayout):
+    ref = None
+
+    def oneTime(self):
+        self.ids.scanBtn.disabled = True
+        self.press()
+
+    def press(self):
+        global local_ip
+        self.ids.acceptBtn.disabled = True
+        self.ids.declineBtn.disabled = True
+        print(local_ip)
+        r.bind((local_ip, 55555))
+
+
+        print("listening: ...")
+        # run waiter in thread
+        waiterThread = threading.Thread(target=self.waiter)
+        waiterThread.start()
+        waiterThread.join()
+
+        # when waiter returns, enable accept/ decline
+        self.ids.acceptBtn.disabled = False
+        self.ids.declineBtn.disabled = False
+        """"""
+
+        # if accept(new function) make the objects, enable cont
+        # if decline(new function) disable acc/dec and call press
+
+    def waiter(self):# waits for connection, when it comes updates client/address and returns to press
+        global client, address
+        r.listen()
+        client_acquired = False
+        while not client_acquired:
+            client, address = r.accept()
+            if len(address) > 0:
+                # update ipLabel
+                self.ids.ipLabel.text = address[0]
+                print("This person is trying to connect to you")
+                print(address)
+                client_acquired = True
+
+    def accepted(self):
+        s.connect((address[0], 55556))
+        self.ids.acceptBtn.disabled = True
+        self.ids.declineBtn.disabled = True
+
+        self.ids.contBtn.disabled = False
+
+
+    def declined(self):
+        self.press()
+        print("declined")
+
+    def dis(self):
+        print(self.ref)
+        self.ref.dismiss()
+
+def show_RecievePop():
+    show = RecievePop()
+
+    popupWindow = Popup(title="Connecting", content=show, size_hint=(None,None),size=(400,400))
+
+    show.ref = popupWindow
+    popupWindow.open()
+
+
+class MyApp(App):
+    def build(self):
+        self.m = Manager(transition=NoTransition())
+
+
+if __name__ == "__main__":
+    MyApp().run()
+    client.close()
+    s.close()
+    r.close()
+
+
+# todo timeout when connecting
+# todo make ai
+# todo change info btn to point to info screen
--- main.py
+++ main.py
@@ -5,32 +5,74 @@ import threading
 # netstat -ano|findstr 55555
 # taskkill /F /PID 12700
 
-# start as soon as app starts
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#s.bind(("127.0.0.1", 55555))
-hostname = socket.gethostname()
-local_ip = socket.gethostbyname(hostname)
-print(local_ip)
-s.bind((local_ip, 55555))
-s.listen()
-print("listening: ...")
+# start as soon as app starts, to pick the connection
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # sender
+r = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # reciever
 
-# conecting to a specific client
-client_acquired = False
-while not client_acquired:
-    client, address = s.accept()
-    if len(address) > 0:
-        print("This person is trying to connect to you")
-        print(address)
-        choice = input("If you would like to accept (1), else (0): ")
-        if int(choice) == 1:
-            client_acquired = True
+def zeroOneChoice(prompt):
+    while True:
+        try:
+            choice = int(input(prompt))
+            if choice != 0 and choice != 1:
+                raise Exception
+
+            return choice
+        except:
+            print("Please enter a 0 or 1")
+
+
+connectionChoice = zeroOneChoice("Would you like to connect(0) or wait for a connection(1): ")
+if connectionChoice == 0:
+            otherIp = input('please enter the other ip: ')
+
+            # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((otherIp, 55555))
+
+            # recieve the connection, making sender socket
+            hostname = socket.gethostname()
+            local_ip = socket.gethostbyname(hostname)
+
+            r.bind((local_ip, 55556))
+            r.listen()
+
+            # conecting to a specific client
+            client_acquired = False
+            while not client_acquired:
+                client, address = r.accept()
+                if len(address) > 0:
+                    if address[0] == otherIp:
+                        client_acquired = True
+
+elif connectionChoice == 1:
+            hostname = socket.gethostname()
+            local_ip = socket.gethostbyname(hostname)
+            print(local_ip)
+
+            # recieve the connection, making sender socket
+            r.bind((local_ip, 55555))
+            r.listen()
+            print("listening: ...")
+
+            # conecting to a specific client
+            client_acquired = False
+            while not client_acquired:
+                client, address = r.accept()
+                if len(address) > 0:
+                    print("This person is trying to connect to you")
+                    print(address)
+                    choice = input("If you would like to accept (1), else (0): ")
+                    if int(choice) == 1:
+                        client_acquired = True
+
+            # sending a request for connection, making reciever socket
+            s.connect((address[0], 55556))
+
+
 
 # sending message
 def sender():
     while True:
-        #client, address = s.accept()
-        print(address)
+        #print(address)
         client.send("you are now conneted ".encode())
         endConnection = False
         while not endConnection:
@@ -40,7 +82,9 @@ def sender():
             client.send(message.encode())
 
         client.close()
+        print("-!-!-!-connection ended-!-!-!-")
         break
+
 
 t1 = threading.Thread(target=sender)
 t1.start()
